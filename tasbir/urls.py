fa838609a52b601b56731a189bfbee6f01ba21ff
Bikalpa Dhakal <theoctober19th@gmail.com>
2019-12-23 10:20:12 +0545

    static url added to root urls.py

--- tasbir/urls.py
+++ tasbir/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('photo_app.urls')),
     path('user/', include('user_app.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
